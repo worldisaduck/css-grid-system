43b3a75497ca10e0ea93351cbf4e97bd65f549b8
Pavel <worldisaduck@gmail.com>
2017-01-08 23:05:26 +0300

    completed assignment

---
+++ angualar_module_1/app.js
@@ -0,0 +1,25 @@
+(function () {
+	'use strict';
+
+	angular.module('LunchCheck', [])
+	.controller('LunchCheckController', LunchCheckControll);
+
+	function LunchCheckControll ($scope) {
+		$scope.dishesList = '';
+		$scope.countDishes = function (string) {
+			var dishesList = string.split(/[\s,]+/);
+			var dishesAmount = dishesList.length;
+			if (dishesList[0] == '') {
+				$scope.message = 'Please enter data first.';
+				$scope.status = 'red';
+			} else if (dishesAmount <= 3) {
+				$scope.message = 'Enjoy!';
+				$scope.status = 'green';
+			} else {
+				$scope.message = 'Too Much!';
+				$scope.status = 'green';
+			}
+		}
+	}
+
+})();
